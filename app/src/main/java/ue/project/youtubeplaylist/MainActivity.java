8b1909dcf70a53ba374cc8c50aa8defb1e11f9b6
Enock Beauchant <beauchant509@gmail.com>
2023-03-16 18:06:49 -0400

    Update

--- app/src/main/java/ue/project/youtubeplaylist/MainActivity.java
+++ app/src/main/java/ue/project/youtubeplaylist/MainActivity.java
@@ -1,14 +1,34 @@
 package ue.project.youtubeplaylist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
 public class MainActivity extends AppCompatActivity {
+    private YouTubePlayerView youtube_player_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        youtube_player_view = findViewById(R.id.youtube_player_view);
+
+        youtube_player_view.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "rGw6XcHUArc";
+                // To play the video auto
+//                youTubePlayer.loadVideo(videoId,0);
+
+                // To play the video when clicking on play
+                youTubePlayer.cueVideo(videoId,0);
+            }
+        });
     }
 }
