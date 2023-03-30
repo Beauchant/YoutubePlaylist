51e18d0c242dab5044e7b5fdac57feab9901e29b
Enock Beauchant <beauchant509@gmail.com>
2023-03-30 11:35:49 -0400

    Update

--- app/src/main/java/ue/project/youtubeplaylist/MainActivity.java
+++ app/src/main/java/ue/project/youtubeplaylist/MainActivity.java
@@ -9,26 +9,42 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    private YouTubePlayerView youtube_player_view;
+
+    ArrayList<String> YtVideoID = new ArrayList<>();
+    ArrayList<YouTubePlayerView> resourcesID = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        youtube_player_view = findViewById(R.id.youtube_player_view);
+        resourcesID.add(findViewById(R.id.youtube_player_view0));
+        resourcesID.add(findViewById(R.id.youtube_player_view1));
+        resourcesID.add(findViewById(R.id.youtube_player_view2));
+        resourcesID.add(findViewById(R.id.youtube_player_view3));
 
-        youtube_player_view.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-            @Override
-            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                String videoId = "rGw6XcHUArc";
-                // To play the video auto
-//                youTubePlayer.loadVideo(videoId,0);
+        YtVideoID.add("rGw6XcHUArc");
+        YtVideoID.add("VRUCHHxQ4Bs");
+        YtVideoID.add("7u3KpSaXNoc");
+        YtVideoID.add("qpoAmKf7YZQ");
 
-                // To play the video when clicking on play
-                youTubePlayer.cueVideo(videoId,0);
-            }
-        });
+        for(int i = 0; i < YtVideoID.size(); i++){
+            int finalI = i;
+            String YPV = "youtube_player_view"+String.valueOf(finalI);
+            resourcesID.get(finalI).addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+                @Override
+                public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                    String videoId = YtVideoID.get(finalI);
+                    // To play the video auto
+                    // youTubePlayer.loadVideo(videoId,0);
+
+                    // To play the video when clicking on play
+                    youTubePlayer.cueVideo(videoId,0);
+                }
+            });
+        }
     }
 }
